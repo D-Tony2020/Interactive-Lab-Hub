0827f59961684f9bb7af63d8d9b1fb2fcfc933c1
D-Tony2020 <hx332@cornell.edu>
2025-11-09 19:06:57 -0500

    Update ledtest.py

--- Lab 6/ledtest.py
+++ Lab 6/ledtest.py
@@ -1,16 +1,11 @@
 from gpiozero import RGBLED
 from time import sleep
 
-led = RGBLED(red=17, green=5, blue=6)
+led = RGBLED(red=17, green=23, blue=26)
 
 while True:
-    led.color = (1,0,0)
-    sleep(1)
-    led.color = (0,1,0)
-    sleep(1)
-    led.color = (0,0,1)
-    sleep(1)
-    led.color = (1,1,1)
-    sleep(1)
+    for color in [(1,0,0),(0,1,0),(0,0,1),(1,1,1)]:
+        led.color = color
+        sleep(1)
     led.off()
     sleep(1)

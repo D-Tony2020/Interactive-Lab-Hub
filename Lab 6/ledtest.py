6e6ddfa22f31639e9070bcfa188b7f62be935cb2
D-Tony2020 <hx332@cornell.edu>
2025-11-09 19:12:17 -0500

    Update ledtest.py

--- Lab 6/ledtest.py
+++ Lab 6/ledtest.py
@@ -6,12 +6,12 @@ led = RGBLED(red=17, green=16, blue=26)
 
 while True:
     led.color = (1, 0, 0)  # 红
-    sleep(1)
+    sleep(3)
     led.color = (0, 1, 0)  # 绿
-    sleep(1)
+    sleep(3)
     led.color = (0, 0, 1)  # 蓝
-    sleep(1)
+    sleep(3)
     led.color = (1, 1, 1)  # 白
-    sleep(1)
+    sleep(3)
     led.off()
-    sleep(1)
+    sleep(5)

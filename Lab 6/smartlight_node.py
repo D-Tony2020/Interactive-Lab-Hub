c51a1cbdf1a84a9591348d4229c1672e23459e32
D-Tony2020 <hx332@cornell.edu>
2025-11-10 19:18:23 -0500

    Update smartlight_node.py

--- Lab 6/smartlight_node.py
+++ Lab 6/smartlight_node.py
@@ -58,6 +58,10 @@ def turn_light(on):
         if on:
             led.color = (1, 0, 0)   # 白灯亮
         else:
+            led.color = (0, 1, 0)
+            sleep(1)
+            led.color = (0, 0, 1)
+            sleep(1)
             led.off()               # 关灯
 
         action = "ON" if on else "OFF"
